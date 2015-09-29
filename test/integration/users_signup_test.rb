dab6640341d8848e281a5d3c5146dbdada289c0b
Andrew Murray <andrew.murray21@yahoo.co.uk>
2015-09-30 01:09:14 +0300

    user authentication, sessions and cookies

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,6 +11,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password_confirmation: "bar" }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.error_border'
+
   end
 
   test "valid signup information" do
@@ -22,5 +25,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "password" }
     end
     assert_template 'users/show'
+    assert_not flash.nil?
+    assert is_logged_in?
   end
 end
